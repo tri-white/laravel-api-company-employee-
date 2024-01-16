6f935f18b63d1b6fbc5ccb70afa2da0ec4654de1
tri-white <olegovic101@gmail.com>
2024-01-16 22:08:11 +0200

    added test for deleting with AJAX request

--- tests/Feature/CompaniesControllerTest.php
+++ tests/Feature/CompaniesControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Companies;
 
 class CompaniesControllerTest extends TestCase
 {
@@ -16,5 +17,18 @@ class CompaniesControllerTest extends TestCase
         $response = $this->get('/api/companies');
 
         $response->assertStatus(200);
+    }
+    use RefreshDatabase;
+
+    public function testDestroy()
+    {
+        $company = Companies::factory()->count(1)->create();
+        $company = Companies::all()->first();
+        $id = $company->id;
+        $ajaxResponse = $this->delete(route('companies.destroy', ['company' => $id]), [], ['HTTP_X-Requested-With' => 'XMLHttpRequest']);
+
+        // Check the response for an AJAX request
+        $ajaxResponse->assertJson(['Deleted'])
+            ->assertStatus(200);
     }
 }
